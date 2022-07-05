bec0984a847f61fa6920d6dec64fc8c37094e75b
Maxim Belov <belov.max7@yandex.ru>
2022-07-05 22:28:17 +0300

    Подкорректировал строку вывода

--- Task_023/Program.cs
+++ Task_023/Program.cs
@@ -1,14 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-
-
-
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N)
+//  и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27  - (Здесь была ошибка)
+// 5 -> 1, 8, 27, 64, 125
 
 Console.WriteLine("Введите число:");
 int n = Convert.ToInt32(Console.ReadLine());
 
 for (int i = 1; i <= n; i++)
 {
-    Console.WriteLine($"|Куб числа {i} =|{i * i*i,4}|");
+    Console.WriteLine($"Куб числа {i} =|{i * i * i,4}|");
 }
